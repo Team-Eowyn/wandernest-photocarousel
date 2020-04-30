f31dba3f5402c9f383bf8b7efdfcc13ec481661a
Ryan Moniz <ryancmoniz@gmail.com>
2020-04-30 19:22:27 -0400

    Implement API endpoints for Hotel controller

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const HotelController = require('../database/controllers/hotel.js');
+
 let app = express();
-
-const mongodb = require('../database/index.js');
-
 app.use(express.static(__dirname + '/../public/'));
-
 let port = 3000;
 
+
+/* Hotels */
+app.get('/api/hotels', (req, res) => {
+  HotelController.getAllHotels((err, results) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+app.get('/api/hotels/:id', (req, res) => {
+  let params = req.params;
+  HotelController.getHotelById(params, (err, results) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+/* Server Start */
 app.listen(port, function () {
   console.log(`Express listening on port [ ${port} ]`);
 });
