94cd12e56a4906bf8e3513c154d3c998dadac4f7
Ryan Moniz <ryancmoniz@gmail.com>
2020-05-01 19:34:11 -0400

    Setup react skeleton and link to index.html

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>If you are reading this webpack and babel have compiled successfully!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
