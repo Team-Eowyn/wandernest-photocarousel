321327ae7bb2d7621aac442d7350143c50b2cafe
Ryan Moniz <ryancmoniz@gmail.com>
2020-05-19 10:37:33 -0400

    Quick fix on brightness of sidebar images

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -17,11 +17,11 @@ const SidebarItem = styled.div`
   justify-content:center;
   text-align: center;
   line-height:1.25;
-  background-image: url(${props => props.img});
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props => props.img});
   background-size:100% 100%;
 
   &:hover {
-    filter: brightness(125%);
+    filter: brightness(110%);
   }
 `
 
